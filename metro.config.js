e9b62810f86c363719e132eed0577e7b2dcb0ef8
xenartist <146194585+xenartist@users.noreply.github.com>
2026-02-13 16:24:40 +1100

    first init

---
+++ metro.config.js
@@ -0,0 +1,21 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  watchFolders: [],
+  watcher: {
+    watchman: {
+      deferStates: ['hg.update'],
+    },
+    healthCheck: {
+      enabled: false,
+    },
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
