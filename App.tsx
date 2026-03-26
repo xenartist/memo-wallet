b58090ad18fb6b1c29d7e11331211290e19ce777
xenartist <146194585+xenartist@users.noreply.github.com>
2026-03-26 15:18:42 +1100

    v1.0.4

--- App.tsx
+++ App.tsx
@@ -1092,7 +1092,7 @@ function App(): JSX.Element {
       </TouchableOpacity>
 
       <Text style={styles.hint}>Powered by X1 & Solana</Text>
-      <Text style={styles.versionText}>v1.0.3</Text>
+      <Text style={styles.versionText}>v1.0.4</Text>
     </View>
   );
 
@@ -1382,7 +1382,7 @@ function App(): JSX.Element {
             Remove Authorization
           </Text>
         </TouchableOpacity>
-        <Text style={styles.settingsVersionText}>v1.0.3</Text>
+        <Text style={styles.settingsVersionText}>v1.0.4</Text>
       </ScrollView>
     </View>
   );
